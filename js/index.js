5fa1661440140a3d8b9d039dab3e0671c8f33750
MatiasGaitan <matiasgaitan547@gmail.com>
2026-04-29 14:38:52 +0200

    feat : se coloca horizontalmente los colores y se repara la funcion del boton generar y selecionar cantidad

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const botonGenerar = document.getElementById("boton-generar");
 const cajaColores = document.getElementById("contenedor-colores")
 const cantidadColores = document.getElementById("cantidad-colores")
-
+let valor = 6
 
 //Numeros aleatorios
 const numeroAleatorio = function(){
@@ -19,23 +19,25 @@ const colorRgb = function(){
     return color
 }
 
-botonGenerar.addEventListener("click",function(){
-    cajaColores.style.backgroundColor= colorRgb()
-})
 
 cantidadColores.addEventListener("change",function(event){
-    const valor = event.target.value
-    cajaColores.innerHTML= "" // Formatea la caja cada vez que seleccionamos un nuevo valor
-
+    valor = event.target.value
+    cajaColores.style.gridTemplateColumns = `repeat(${valor},1fr)`   // Ubica cantidad de columnas necesarias segun lo seleccionado 
+    cajaColores.innerHTML= ""   // Formatea la caja cada vez que seleccionamos un nuevo valor
+    
     for (let i = 0 ; i<valor ;i++ ) {
-        const div = document.createElement("div")     // Usamos bucle for para crear la cantidad de "hijos" necesarios y pintarlos
+        const div = document.createElement("div")     // Crea la cantidad de hijos necesarios y los agrega 
         cajaColores.appendChild(div)
-
+        
     }
-    const cantidad = cajaColores.children.length
-    for (let i = 0 ; 1< (cantidad); i++){                              // FALTA OPTIMIZAR MEJORAR AUN QUE SE CAMBIE CON GENERAR Y VARIAR 
+}) 
+
+
+
+botonGenerar.addEventListener("click",function(){
+
+    for (let i = 0 ; i < Number(valor); i++){                          // Da los colores a cada hijo 
         cajaColores.children[i].style.backgroundColor = colorRgb()
-
+    
     }
-
 })
