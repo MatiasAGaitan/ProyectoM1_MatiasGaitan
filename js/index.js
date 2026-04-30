92a78fac1aa0195450b0c7ed9d9adc138de648dd
MatiasGaitan <matiasgaitan547@gmail.com>
2026-04-30 20:16:15 +0200

    feat : agrego nuevos enventos, catalogo con comentarios el codigo

--- js/index.js
+++ js/index.js
@@ -1,43 +1,66 @@
+//SELECCIONANDO ELEMENTOS
+const botonGenerar = document.querySelector(".boton-generar");
+const contenedorColores = document.querySelector(".contenedor-colores")
+const seleccionCantidad = document.querySelector(".seleccion-cantidad")
 
-const botonGenerar = document.getElementById("boton-generar");
-const cajaColores = document.getElementById("contenedor-colores")
-const cantidadColores = document.getElementById("cantidad-colores")
-let valor = 6
+let valor 
+let x = []
 
-//Numeros aleatorios
-const numeroAleatorio = function(){
-    let numero= Math.floor(Math.random()*250)
-    return numero
+// FUNCION GENERADORA DE COLORES HLS
+const colorHsl = function(){
+    let color1 = Math.floor(Math.random()*250)
+    let color2 = Math.floor(Math.random()*100)
+    let color3 = Math.floor(Math.random()*100)
+
+    const color = `hsl(${color1},${color2}%,${color3}%)`
+    return color
 }
-//Generador de RGB
 const colorRgb = function(){
-    let color1 = numeroAleatorio()
-    let color2 = numeroAleatorio()
-    let color3 = numeroAleatorio()
+    let color1 = Math.floor(Math.random()*255)
+    let color2 = Math.floor(Math.random()*255)
+    let color3 = Math.floor(Math.random()*255)
 
     const color = `rgb(${color1},${color2},${color3})`
     return color
 }
 
 
-cantidadColores.addEventListener("change",function(event){
-    valor = event.target.value
-    cajaColores.style.gridTemplateColumns = `repeat(${valor},1fr)`   // Ubica cantidad de columnas necesarias segun lo seleccionado 
-    cajaColores.innerHTML= ""   // Formatea la caja cada vez que seleccionamos un nuevo valor
-    
+/*EVENTO AL SELECCIONAR UNA OPOCION (CANTIDAD DE COLORES) */
+seleccionCantidad.addEventListener("change",function(event){
+    contenedorColores.innerHTML= ""
+    valor = Number(event.target.value)
+    contenedorColores.style.gridTemplateColumns = `repeat(${valor},1fr)`   // Ubica cantidad de columnas necesarias segun lo seleccionado
+
     for (let i = 0 ; i<valor ;i++ ) {
-        const div = document.createElement("div")     // Crea la cantidad de hijos necesarios y los agrega 
-        cajaColores.appendChild(div)
-        
+        const hijoColor = document.createElement("div")                   // Crea la cantidad de hijos necesarios y los agrega
+        hijoColor.classList.add("color")
+        contenedorColores.appendChild(hijoColor)   
     }
 }) 
 
-
-
+// EVENTO CLICK EN GENERAR
 botonGenerar.addEventListener("click",function(){
+    for (let i = 0 ; i < valor; i++){                     
+        contenedorColores.children[i].style.backgroundColor = colorHsl()  
+        }
+    })
 
-    for (let i = 0 ; i < Number(valor); i++){                          // Da los colores a cada hijo 
-        cajaColores.children[i].style.backgroundColor = colorRgb()
+// EVENTO CUANDO EL MOUSE ENTRA AL COLOR
+contenedorColores.addEventListener("mouseover",function(event){
+    if(event.target.className === "color"){
+        const caractColor = document.createElement("div")
+        caractColor.classList.add("caractColor")
+        caractColor.textContent = "COLOR"
     
     }
 })
+
+// EVENTO CUANDO EL MOUSE SALE DEL COLOR
+contenedorColores.addEventListener("mouseout",function(event){
+    /* console.log(contenedorColores.children[0].style.backgroundColor) */
+    if(event.target.className === "color"){
+        event.target.textContent = ""
+    }
+})
+
+
