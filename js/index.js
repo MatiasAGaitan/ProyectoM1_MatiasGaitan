8b47aaab62604d64b51ba0dd740c522093041fc3
MatiasGaitan <matiasgaitan547@gmail.com>
2026-04-30 23:51:03 +0200

    feat : agrego y mejoro funciones de colores / fix : reparo error en los eventos click y change

--- js/index.js
+++ js/index.js
@@ -3,49 +3,56 @@ const botonGenerar = document.querySelector(".boton-generar");
 const contenedorColores = document.querySelector(".contenedor-colores")
 const seleccionCantidad = document.querySelector(".seleccion-cantidad")
 
-let valor 
-let x = []
+let valor
+
+// GENERADOR DE NUMEROS ALEATORIOS
+const numeroAleatorio = function(limite){
+    numero = Math.floor(Math.random()*limite)
+    return numero
+}
 
 // FUNCION GENERADORA DE COLORES HLS
 const colorHsl = function(){
-    let color1 = Math.floor(Math.random()*250)
-    let color2 = Math.floor(Math.random()*100)
-    let color3 = Math.floor(Math.random()*100)
-
-    const color = `hsl(${color1},${color2}%,${color3}%)`
+    const color = `hsl(${numeroAleatorio(250)},${numeroAleatorio(100)}%,${numeroAleatorio(100)}%)`
     return color
 }
 const colorRgb = function(){
-    let color1 = Math.floor(Math.random()*255)
-    let color2 = Math.floor(Math.random()*255)
-    let color3 = Math.floor(Math.random()*255)
-
-    const color = `rgb(${color1},${color2},${color3})`
+    const color = `rgb(${numeroAleatorio(255)},${numeroAleatorio(255)},${numeroAleatorio(255)})`
     return color
 }
 
 
-/*EVENTO AL SELECCIONAR UNA OPOCION (CANTIDAD DE COLORES) */
-seleccionCantidad.addEventListener("change",function(event){
-    contenedorColores.innerHTML= ""
-    valor = Number(event.target.value)
-    contenedorColores.style.gridTemplateColumns = `repeat(${valor},1fr)`   // Ubica cantidad de columnas necesarias segun lo seleccionado
-
-    for (let i = 0 ; i<valor ;i++ ) {
-        const hijoColor = document.createElement("div")                   // Crea la cantidad de hijos necesarios y los agrega
-        hijoColor.classList.add("color")
-        contenedorColores.appendChild(hijoColor)   
-    }
-}) 
 
 // EVENTO CLICK EN GENERAR
 botonGenerar.addEventListener("click",function(){
-    for (let i = 0 ; i < valor; i++){                     
-        contenedorColores.children[i].style.backgroundColor = colorHsl()  
+    valor = seleccionCantidad.value
+    contenedorColores.innerHTML= ""
+    contenedorColores.style.gridTemplateColumns = `repeat(${valor},1fr)`   // Ubica cantidad de columnas necesarias segun lo seleccionado
+    
+    
+    for (let i = 0 ; i < valor; i++){    
+        let color = colorHsl()
+        const hijoColor = document.createElement("div")                    // Crea hijo 
+        hijoColor.classList.add("color")                                  // Agrega clase al contenedor 
+        hijoColor.style.backgroundColor = color
+
+        const descripcionColor = document.createElement("p")               // Crea descripcion 
+        descripcionColor.classList.add("descripcion-color")               // Agrega clase al contenedor
+        descripcionColor.textContent = color
+
+
+        contenedorColores.appendChild(hijoColor)                          // Agrega el hijo al padre
+        hijoColor.appendChild(descripcionColor)
+        
         }
     })
 
-// EVENTO CUANDO EL MOUSE ENTRA AL COLOR
+
+
+
+
+
+/* // EVENTO CUANDO EL MOUSE ENTRA AL COLOR
 contenedorColores.addEventListener("mouseover",function(event){
     if(event.target.className === "color"){
         const caractColor = document.createElement("div")
@@ -54,13 +61,14 @@ contenedorColores.addEventListener("mouseover",function(event){
     
     }
 })
+ */
 
+/* 
 // EVENTO CUANDO EL MOUSE SALE DEL COLOR
 contenedorColores.addEventListener("mouseout",function(event){
-    /* console.log(contenedorColores.children[0].style.backgroundColor) */
     if(event.target.className === "color"){
         event.target.textContent = ""
     }
 })
-
+*/
 
