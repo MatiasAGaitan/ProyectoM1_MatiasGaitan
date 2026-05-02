f6227171004020f563b05c71a5dcd9d48d474435
MatiasGaitan <matiasgaitan547@gmail.com>
2026-05-03 01:51:21 +0200

    feat : creo la funcion creaElementos para simplificar codiog - se creo nuevamente el event click generar para agrgar el funcionamiento del boton de bloqueado - creo el mensaje para el usuario realizado con exito

--- js/index.js
+++ js/index.js
@@ -2,66 +2,107 @@
 const botonGenerar = document.querySelector(".boton-generar");
 const contenedorColores = document.querySelector(".contenedor-colores")
 const seleccionCantidad = document.querySelector(".seleccion-cantidad")
+const contenedorMain = document.querySelector(".contenedor-main")
 
-let valor
 
 // GENERADOR DE NUMEROS ALEATORIOS
 const numeroAleatorio = function(limite){
     numero = Math.floor(Math.random()*limite)
     return numero
 }
-
 // FUNCION GENERADORA DE COLORES HLS
 const colorHsl = function(){
-    const color = `hsl(${numeroAleatorio(250)},${numeroAleatorio(100)}%,${numeroAleatorio(100)}%)`
+    const color = `hsl(${numeroAleatorio(360)},${numeroAleatorio(100)}%,${numeroAleatorio(100)}%)`
     return color
 }
 const colorRgb = function(){
     const color = `rgb(${numeroAleatorio(255)},${numeroAleatorio(255)},${numeroAleatorio(255)})`
     return color
 }
-
+// Funcion para crear elementos
+const crearElemento = function(elemento,clase){
+    const nombre = document.createElement(String(elemento))                   
+    nombre.classList.add(String(clase))
+    return nombre
+}
 
 
 // EVENTO CLICK EN GENERAR
 botonGenerar.addEventListener("click",function(){
-    valor = seleccionCantidad.value
-    contenedorColores.innerHTML= ""
+    const valor = Number(seleccionCantidad.value)
+    
     contenedorColores.style.gridTemplateColumns = `repeat(${valor},1fr)`   // Ubica cantidad de columnas necesarias segun lo seleccionado
     
-    
-    for (let i = 0 ; i < valor; i++){    
-        let color = colorHsl()
-        
-        // Crea el div del color - le agrega clase - da color
-        const cajaColor = document.createElement("div")                   
-        cajaColor.classList.add("color")                                  
-        cajaColor.style.backgroundColor = color
+    //MENSAJE USUARIO
+    mensajeUsuario.style.display = "flex"
 
-        // Crea el parrafo - le agrega la clase - agrega el contenido a mostrar en p
-        const descripcionColor = document.createElement("p")               
-        descripcionColor.classList.add("descripcion-color")               
-        descripcionColor.textContent = color
-
-        // Agrega dentro de cada padre a su hijo
-        contenedorColores.appendChild(cajaColor)                          
-        cajaColor.appendChild(descripcionColor)
-        }
-
-    // Crea el elemento p que va a ser mostrado despues de generar los colores
-    const mensajeUsuario = document.createElement("p")
-    mensajeUsuario.classList.add("mensaje-usuario")
-    mensajeUsuario.textContent = "PALETA DE COLORES GENERADA CON EXITO🚀✔️"
-    contenedorColores.appendChild(mensajeUsuario)
-    
-    // esta funcion hace que depues de cierto tiempo x cosa suceda 
     setTimeout(function(){
-        mensajeUsuario.style.display = "none"
+        mensajeUsuario.style.display = "none"       // ejecuta la funcion despues de x tiempo (es milisegundos)
     },1500)
 
+    // si los hijos existen verificar candado
+    if (contenedorColores.children.length === valor){
+        
 
-    })
+        for (let i = 0 ; i < valor; i++){
+            
+            //Los hijos existen "retomaos" sus datos 
+            const caja = contenedorColores.children[i];
+            const candado = caja.querySelector(".candado");
+            const descripcionColor = caja.querySelector("p")
 
+            //Verificacion de candado
+            if (candado.classList.contains("desbloqueado")){
+                const nuevoColor = colorHsl()
+                caja.style.backgroundColor = nuevoColor;
+                descripcionColor.textContent = nuevoColor
+            }
+            
+        }}else{
+            //Formateo contenedor
+            contenedorColores.innerHTML= ""
+
+        for (let i = 0 ; i < valor; i++){    
+            const color = colorHsl()
+            
+            // COLOR
+            const cajaColor = crearElemento("div","color")
+            cajaColor.style.backgroundColor = color
+            contenedorColores.appendChild(cajaColor)
+
+            //CANDADO
+            const candado = crearElemento("img","candado")
+            candado.src ="../img/abierto.png"
+            candado.classList.add("desbloqueado")
+            cajaColor.appendChild(candado)
+
+            //CLICK CANDADO
+            candado.addEventListener("click",function(){
+                    // si candado desbloqueado lo bloqueamos
+                if (candado.classList.contains('desbloqueado')) {
+                    candado.classList.remove("desbloqueado")
+                    candado.src = "../img/cerrado.png"
+                } else { // si esta bloqueado lo desbloqueamos
+                    candado.classList.add("desbloqueado")
+                    candado.src = "../img/abierto.png"
+                }})                           
+            
+
+            // TEXTO
+            const descripcionColor = crearElemento("p","descripcion-color")              
+            descripcionColor.textContent = color
+            cajaColor.appendChild(descripcionColor)
+            
+
+
+
+        }}})
+
+//MENSAJE USUARIO
+mensajeUsuario = crearElemento("p","mensaje-usuario")
+mensajeUsuario.textContent = "PALETA DE COLORES GENERADA CON EXITO🚀✔️"
+mensajeUsuario.style.display = "none"   // none "hace desaparecer nuestro elemento"
+contenedorMain.appendChild(mensajeUsuario)
 
 
 
